fa87429b8a8757e9138c1e99f7745c3e4dacab8e
Remi Mendoza <mendozaremi@gmail.com>
2020-08-15 03:10:51 -0400

    working on hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav.js';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
-        {/* Hero About Bjorn*/}
+        <Hero />
         {/* Books  */}
         {/* Footer */}
       </header>
