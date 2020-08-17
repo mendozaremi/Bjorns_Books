b65ba2b8c8c78b04530d61297600d1f8c66dc3bc
Remi Mendoza <mendozaremi@gmail.com>
2020-08-17 01:13:47 -0400

    styled navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav.js';
-import Hero from './Hero';
+// import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
-        <Hero />
+        {/* <Hero /> */}
         {/* Books  */}
         {/* Footer */}
       </header>
--- src/Nav.js
+++ src/Nav.js
@@ -4,11 +4,23 @@ import './Nav.css';
 function Nav() {
   return (
     <div className="nav">
-      <button className="nav_logo">Logo</button>
-      <button>Best Sellers</button>
-      <button>Björns Books</button>
-      <button>Account</button>
-      <button className="nav_cart">Cart</button>
+      <img
+        className="nav_logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSsclNX7hVP2_PZmIoMav7sKtXPN32t-mrQzA&usqp=CAU"
+        alt="book"
+      />
+      <button className="nav_browser">Browse</button>
+      <button className="nav_bestsellers">Best Sellers</button>
+      <img
+        className="nav_account"
+        src="https://toppng.com/uploads/preview/user-account-management-logo-user-icon-11562867145a56rus2zwu.png"
+        alt="account"
+      />
+      <img
+        className="nav_cart"
+        src="https://www.canadianginsengfarm.com/wp-content/uploads/2018/03/Shopping-Cart-PNG.png"
+        alt="cart"
+      />
     </div>
   );
 }
