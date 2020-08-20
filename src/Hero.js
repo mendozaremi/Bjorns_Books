76612204310d561388a9cb53df628e694235db29
Remi Mendoza <mendozaremi@gmail.com>
2020-08-19 21:26:14 -0400

    working on hero styling and responsiveness

--- src/Hero.js
+++ src/Hero.js
@@ -3,16 +3,17 @@ import './Hero.css';
 
 function Hero() {
   return (
-    <div className="hero">
-      <div>
+    <div className="hero_container">
+      <div className="hero_image_container">
         <img
           className="hero_image"
           src="https://i.imgur.com/KBPsiYF.jpg"
           alt="Björn img"
         />
       </div>
-      <div>
-        <p className="hero_bio">
+      <div className="hero_bio">
+        <h1>HI I'M BJÖRN</h1>
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
