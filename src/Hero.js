f15e8cfeaef1ff0248c1b07dd706958fef4100a6
Remi Mendoza <mendozaremi@gmail.com>
2020-08-19 21:01:59 -0400

    added to hero

--- src/Hero.js
+++ src/Hero.js
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import './Hero.css';
+import React from 'react';
+import './Hero.css';
 
-// function Hero() {
-//   return (
-//     <div className="hero">
-//       <div>
-//         <img
-//           className="hero_image"
-//           src="https://i.imgur.com/KBPsiYF.jpg"
-//           alt="Björn img"
-//         />
-//       </div>
-//       <div>
-//         <p className="hero_bio">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//           aliquip ex ea commodo consequat.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
+function Hero() {
+  return (
+    <div className="hero">
+      <div>
+        <img
+          className="hero_image"
+          src="https://i.imgur.com/KBPsiYF.jpg"
+          alt="Björn img"
+        />
+      </div>
+      <div>
+        <p className="hero_bio">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+}
 
-// export default Hero;
+export default Hero;
