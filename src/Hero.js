d470ce34874c004517690d79ecc3600cbfc3704f
Remi Mendoza <mendozaremi@gmail.com>
2020-08-22 12:56:23 -0400

    changing up navbar

--- src/Hero.js
+++ src/Hero.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './Hero.css';
+// import React from 'react';
+// import './Hero.css';
 
-function Hero() {
-  return (
-    <div className="hero_container">
-      <div className="hero_image_container">
-        <img
-          className="hero_image"
-          src="https://i.imgur.com/KBPsiYF.jpg"
-          alt="Björn img"
-        />
-      </div>
-      <div className="hero_bio">
-        <h1>HI I'M BJÖRN</h1>
-        <p>
-          This website is to show you great kid's books and my persoanl
-          favorites! My uncle Remi designed this website to show off some of the
-          awesome books I wish I had.
-        </p>
-      </div>
-    </div>
-  );
-}
+// function Hero() {
+//   return (
+//     <div className="hero_container">
+//       <div className="hero_image_container">
+//         <img
+//           className="hero_image"
+//           src="https://i.imgur.com/KBPsiYF.jpg"
+//           alt="Björn img"
+//         />
+//       </div>
+//       <div className="hero_bio">
+//         <h1>HI I'M BJÖRN</h1>
+//         <p>
+//           This website is to show you great kid's books and my persoanl
+//           favorites! My uncle Remi designed this website to show off some of the
+//           awesome books I wish I had.
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Hero;
+// export default Hero;
--- src/Nav.js
+++ src/Nav.js
@@ -3,25 +3,21 @@ import './Nav.css';
 
 function Nav() {
   return (
-    <div className="nav">
-      <img
-        className="nav_logo"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSsclNX7hVP2_PZmIoMav7sKtXPN32t-mrQzA&usqp=CAU"
-        alt="book"
-      />
-      <button className="nav_browser">Browse</button>
-      <button className="nav_bestsellers">Best Sellers</button>
-      <img
-        className="nav_account"
-        src="https://toppng.com/uploads/preview/user-account-management-logo-user-icon-11562867145a56rus2zwu.png"
-        alt="account"
-      />
-      <img
-        className="nav_cart"
-        src="https://www.canadianginsengfarm.com/wp-content/uploads/2018/03/Shopping-Cart-PNG.png"
-        alt="cart"
-      />
-    </div>
+    <header className="nav_container">
+      <h4 className="nav_logo">Björn's Books</h4>
+      <ul className="nav_links">
+        <li>
+          <a href="#">Catalogue</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Cart</a>
+        </li>
+      </ul>
+      <div></div>
+    </header>
   );
 }
 
